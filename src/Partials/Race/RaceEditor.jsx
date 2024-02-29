0a74aea6f82b9cfc6d2ba39c788a607cfad6ac0f
Ken <kel89@cornell.edu>
2024-02-29 14:06:11 -0700

    race editor working

--- src/Partials/Race/RaceEditor.jsx
+++ src/Partials/Race/RaceEditor.jsx
@@ -3,24 +3,31 @@ import TextField from "@mui/material/TextField";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { generateClient } from "@aws-amplify/api";
 import { updateRace } from "../../graphql/mutations";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs, {Dayjs} from 'dayjs';
 
 export default function RaceEditor({ raceData, getRaceData }) {
   const apiClient = generateClient();
 
   const [loading, setLoading] = useState(false);
-  const [date, setDate] = useState(raceData.date);
+  const [date, setDate] = useState(dayjs(raceData.date));
   const [country, setCountry] = useState(raceData.country);
   const [city, setCity] = useState(raceData.city);
   const [raceName, setRaceName] = useState(raceData.name);
 
   const updateData = async () => {
     setLoading(true);
+
+    const dateString = date.toISOString();
+
     const result = await apiClient.graphql({
       query: updateRace,
       variables: {
         input: {
           id: raceData.id,
-          date: date,
+          date: dateString,
           country: country,
           city: city,
           name: raceName,
@@ -43,52 +50,64 @@ export default function RaceEditor({ raceData, getRaceData }) {
     setRaceName(val.target.value);
   };
 
+  const updateDate = (val) => {
+    setDate(val);
+  }
+
   return (
-    <div className="bg-white p-4 border shadow-lg">
-      <h1 className="text-xl text-gray-500 font-racing pb-2">Edit Race</h1>
-      <div className="pb-4">
-        <TextField
-          fullWidth
-          id="newCountry"
-          variant="standard"
-          label="Country"
-          value={country}
-          disabled={loading}
-          onChange={updateCountryText}
-        />
-      </div>
-      <div className="pb-4">
-        <TextField
-          fullWidth
-          id="newCity"
-          variant="standard"
-          label="City"
-          value={city}
-          disabled={loading}
-          onChange={updateCityText}
-        />
-      </div>
-      <div className="pb-4">
-        <TextField
-          fullWidth
-          id="newRaceName"
-          variant="standard"
-          label="Date"
-          value={raceName}
-          disabled={loading}
-          onChange={updateRaceNameText}
-        />
-      </div>
-      <div className='max-w-xs'>
-        <LoadingButton
-          loading={loading}
-          variant='outlined'
-          color='success'
-          onClick={updateData}>
-            Update
-        </LoadingButton>
-      </div>
-      <button onClick={() => console.log(date)}>Click me</button>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div className="bg-white p-4 border shadow-lg">
+          <h1 className="text-xl text-gray-500 font-racing pb-2">Edit Race</h1>
+          <div className="pb-4">
+            <TextField
+              fullWidth
+              id="newCountry"
+              variant="standard"
+              label="Country"
+              value={country}
+              disabled={loading}
+              onChange={updateCountryText}
+            />
+          </div>
+          <div className="pb-4">
+            <TextField
+              fullWidth
+              id="newCity"
+              variant="standard"
+              label="City"
+              value={city}
+              disabled={loading}
+              onChange={updateCityText}
+            />
+          </div>
+          <div className="pb-4">
+            <TextField
+              fullWidth
+              id="newRaceName"
+              variant="standard"
+              label="Date"
+              value={raceName}
+              disabled={loading}
+              onChange={updateRaceNameText}
+            />
+          </div>
+          <div className='pb-4'>
+            <DateTimePicker 
+            fullWidth
+            value={date}
+            onChange={updateDate}
+            />
+          </div>
+          <div className='max-w-xs'>
+            <LoadingButton
+              loading={loading}
+              variant='outlined'
+              color='success'
+              onClick={updateData}>
+                Update
+            </LoadingButton>
+          </div>
+        </div>
+    </LocalizationProvider>
   );
 }
